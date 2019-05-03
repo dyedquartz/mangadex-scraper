ef62baa480878abc9bfccf50c168752a10bc86e6
dyedquartz <dyedquartz@gmail.com>
2019-05-03 09:26:36 -0500

    did part of code that determines file prefix

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,37 @@ use std::fs::File;
 use std::io;
 
 
-fn main() {
+fn main() -> Result<(), reqwest::UrlError> {
     let args: Vec<String> = env::args().collect();
-    let mut resp = reqwest::get(format!("https://s5.mangadex.org/data/{}/{}.png","re", "re")).expect("reeeee");
+
+    let id: &str = &args[1];
+
+    println!("{:?}", args);
+    println!("{}", id);
+
+    if args.len() != 3 { panic!("mangadex-splice <id> <amount>"); }
+
+    let base_url = Url::parse("https://s5.mangadex.org/data/")?;
+    let prefixes = vec!["", "x", "s"];
+
+    let mut pre = String::new();
+
+    for prefix in prefixes {
+        let url = base_url.join(&format!("{}/{}1.png",id, prefix))?;
+        // println!("{:?}", url);
+        let resp = reqwest::get(url).unwrap();
+        if resp.status() == reqwest::StatusCode::OK {
+            pre = String::from(prefix);
+            break;
+        }
+        // println!("{:?}",resp.status());
+    }
+
+    println!("File Prefix: {}", pre);
+    /*
+    let mut resp = reqwest::get(format!("https://s5.mangadex.org/data/{}/{}.png","re", "re"))?;
     let mut out = File::create(format!("{}.png", "1")).expect("failed to create file");
     io::copy(&mut resp, &mut out).expect("failed to copy");
+    */
+    Ok(())
 }
