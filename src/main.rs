d20a2d3bdee315886f36fa473eeafbb47c6bbcd9
dyedquartz <dyedquartz@gmail.com>
2019-05-04 00:28:13 -0500

    changed get to use reqwest::Client for speed

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ fn main() -> Result<(), reqwest::UrlError> {
     // command line arguments
     let args: Vec<String> = env::args().collect();
 
-    let id: &str = &args[1];
-
-    println!("{:?}", args);
-    println!("{}", id);
-
     if args.len() != 4 { panic!("mangadex-splice <id> <amount> <output>"); }
 
     let base_url = Url::parse("https://s2.mangadex.org/data/")?;
     let prefixes = vec!["", "x", "s"];
+    let id: &str = &args[1];
+    println!("{:?}", args);
+    println!("{}", id);
+
+    let client = reqwest::Client::new();
     
     // testing id directory
     let url = base_url.join(id)?;
-    let resp = reqwest::get(url).unwrap();
+    let resp = client.get(url).send().unwrap();
     match resp.status() {
         reqwest::StatusCode::FORBIDDEN => println!("Correct ID Path"),
         reqwest::StatusCode::NOT_FOUND => panic!("Incorrect ID Path"),
@@ -37,7 +37,7 @@ fn main() -> Result<(), reqwest::UrlError> {
     for prefix in prefixes {
         let url = base_url.join(&format!("{}/{}1.png",id, prefix))?;
         println!("{:?}", url);
-        let resp = reqwest::get(url).unwrap();
+        let resp = client.get(url).send().unwrap();
         if resp.status() == reqwest::StatusCode::OK {
             pre = String::from(prefix);
             break;
@@ -57,7 +57,7 @@ fn main() -> Result<(), reqwest::UrlError> {
 
         fs::create_dir_all(format!("{}", args[3])).unwrap();
         let url = base_url.join(&format!("{}/{}{}.png",id, pre, i))?;
-        let mut resp = reqwest::get(url).unwrap();
+        let mut resp = client.get(url).send().unwrap();
         let mut out = File::create(format!("{}/{}.png", args[3], f)).expect("failed to create file");
         io::copy(&mut resp, &mut out).expect("failed to copy");
     }
