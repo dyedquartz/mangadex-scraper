a98acf39a685c11c2e9a7b0be3318995a6aa117c
dyedquartz <dyedquartz@gmail.com>
2019-06-09 17:16:27 -0500

    moved the downloading of chapters to a function.

--- src/main.rs
+++ src/main.rs
@@ -53,39 +53,110 @@ fn main() -> Result<(), reqwest::UrlError> {
         )
         .get_matches();
     let client = reqwest::Client::new();
+
     // getting subcommand higynx
     if let Some(manga) = args.subcommand_matches("manga") {
         let manga_data = mangadex_api::get_manga_data(&client, args.value_of("id").unwrap());
         println!("Scraping '{}'", manga_data.manga.title);
 
-        for (name, data) in manga_data.chapter {
+        for (name, data) in &manga_data.chapter {
             if manga.is_present("lang") {
                 if data.lang_code != manga.value_of("lang").unwrap() {
                     continue;
                 }
             }
-            println!(
-                "{}: volume {} chapter {} in {} from {}",
-                name, data.volume, data.chapter, data.lang_code, data.group_name
-            );
-            let chapter_data = mangadex_api::get_chapter_data(&client, &name);
-            println!("{:#?}", chapter_data);
-            let mut buffer = Vec::new();
-            let options = zip::write::FileOptions::default();
-            let mut archive = File::create(strip_characters(
-                &*format!(
-                    "{} Vol. {} Ch. {} - {} ({}).cbz",
-                    manga_data.manga.title,
+            download_chapter(&client, name.to_string(), data, &manga_data);
+        }
+    }
+    Ok(())
+}
+
+fn strip_characters(original: &str, to_strip: &str) -> String {
+    original
+        .chars()
+        .filter(|&c| !to_strip.contains(c))
+        .collect()
+}
+
+fn download_chapter(
+    client: &reqwest::Client,
+    name: String,
+    data: &mangadex_api::Chapter,
+    manga_data: &mangadex_api::MangaData,
+) {
+    println!(
+        "{}: volume {} chapter {} in {} from {}",
+        name, data.volume, data.chapter, data.lang_code, data.group_name
+    );
+    let chapter_data = mangadex_api::get_chapter_data(&client, &name);
+    println!("{:#?}", chapter_data);
+    let mut buffer = Vec::new();
+    let options = zip::write::FileOptions::default();
+    let mut archive = File::create(strip_characters(
+        &*format!(
+            "{} Vol. {} Ch. {} - {} ({}).cbz",
+            manga_data.manga.title, data.volume, data.chapter, data.group_name, data.lang_code
+        ),
+        "/",
+    ))
+    .expect("failure to create archive");
+    let mut writer = zip::write::ZipWriter::new(&mut archive);
+    for page in chapter_data.page_array {
+        let url = if chapter_data.server == "/data/" {
+            reqwest::Url::parse(&*format!(
+                "https://mangadex.org/data/{}/{}",
+                chapter_data.hash, page
+            ))
+            .unwrap()
+        } else {
+            reqwest::Url::parse(&*format!(
+                "{}{}/{}",
+                chapter_data.server, chapter_data.hash, page
+            ))
+            .unwrap()
+        };
+        println!("downloading {}", &url);
+        let mut resp = client.get(url).send().unwrap();
+        fs::create_dir_all(strip_characters(
+            &*format!(
+                "{} Vol. {} Ch. {} - {} ({})",
+                manga_data.manga.title, data.volume, data.chapter, data.group_name, data.lang_code
+            ),
+            "/",
+        ))
+        .unwrap();
+        let mut out = File::create(
+            std::path::Path::new(&*strip_characters(                &*format!(
+                    "{} Vol. {} Ch. {} - {} ({})",                    manga_data.manga.title,
                     data.volume,
                     data.chapter,
                     data.group_name,
-                    data.lang_code
-                ),
+                    data.lang_code,                ),
                 "/",
             ))
-            .expect("failure to create archive");
-            let mut writer = zip::write::ZipWriter::new(&mut archive);
-            for page in chapter_data.page_array {
+            .join(&page),
+        )
+        .expect("failure to create image");
+        let copy = io::copy(&mut resp, &mut out);
+        let copy = match copy {
+            Ok(file) => file,
+            Err(error) => {
+                println!("Error Copying to File, trying again: {}", error);
+                std::fs::remove_file(
+                    std::path::Path::new(&*strip_characters(
+                        &*format!(
+                            "{} Vol. {} Ch. {} - {} ({})",
+                            manga_data.manga.title,
+                            data.volume,
+                            data.chapter,
+                            data.group_name,
+                            data.lang_code,
+                        ),
+                        "/",
+                    ))
+                    .join(&page),
+                )
+                .unwrap();
                 let url = if chapter_data.server == "/data/" {
                     reqwest::Url::parse(&*format!(
                         "https://mangadex.org/data/{}/{}",
@@ -129,103 +200,28 @@ fn main() -> Result<(), reqwest::UrlError> {
                 )
                 .expect("failure to create image");
                 let copy = io::copy(&mut resp, &mut out);
-                let copy = match copy {
-                    Ok(file) => file,
-                    Err(error) => {
-                        println!("Error Copying to File, trying again: {}", error);
-                        std::fs::remove_file(
-                            std::path::Path::new(&*strip_characters(
-                                &*format!(
-                                    "{} Vol. {} Ch. {} - {} ({})",
-                                    manga_data.manga.title,
-                                    data.volume,
-                                    data.chapter,
-                                    data.group_name,
-                                    data.lang_code,
-                                ),
-                                "/",
-                            ))
-                            .join(&page),
-                        )
-                        .unwrap();
-
-                        let url = if chapter_data.server == "/data/" {
-                            reqwest::Url::parse(&*format!(
-                                "https://mangadex.org/data/{}/{}",
-                                chapter_data.hash, page
-                            ))
-                            .unwrap()
-                        } else {
-                            reqwest::Url::parse(&*format!(
-                                "{}{}/{}",
-                                chapter_data.server, chapter_data.hash, page
-                            ))
-                            .unwrap()
-                        };
-                        println!("downloading {}", &url);
-                        let mut resp = client.get(url).send().unwrap();
-                        fs::create_dir_all(strip_characters(
-                            &*format!(
-                                "{} Vol. {} Ch. {} - {} ({})",
-                                manga_data.manga.title,
-                                data.volume,
-                                data.chapter,
-                                data.group_name,
-                                data.lang_code
-                            ),
-                            "/",
-                        ))
-                        .unwrap();
-                        let mut out = File::create(
-                            std::path::Path::new(&*strip_characters(
-                                &*format!(
-                                    "{} Vol. {} Ch. {} - {} ({})",
-                                    manga_data.manga.title,
-                                    data.volume,
-                                    data.chapter,
-                                    data.group_name,
-                                    data.lang_code,
-                                ),
-                                "/",
-                            ))
-                            .join(&page),
-                        )
-                        .expect("failure to create image");
-                        let copy = io::copy(&mut resp, &mut out);
-                        0
-                    }
-                };
-                println!("compressing {}", &page);
-                let mut image = File::open(
-                    std::path::Path::new(&*strip_characters(
-                        &*format!(
-                            "{} Vol. {} Ch. {} - {} ({})",
-                            manga_data.manga.title,
-                            data.volume,
-                            data.chapter,
-                            data.group_name,
-                            data.lang_code,
-                        ),
-                        "/",
-                    ))
-                    .join(&page),
-                )
-                .unwrap();
-                image.read_to_end(&mut buffer).unwrap();
-                writer.start_file(&*page, options).unwrap();
-                writer.write_all(&*buffer).unwrap();
-                buffer.clear();
-                println!("compressed {}", &page);
-            }
-            writer.finish().unwrap();
-        }
+                0            }
+        };
+        println!("compressing {}", &page);
+        let mut image = File::open(
+            std::path::Path::new(&*strip_characters(
+                &*format!(
+                    "{} Vol. {} Ch. {} - {} ({})",
+                    manga_data.manga.title,
+                    data.volume,
+                    data.chapter,
+                    data.group_name,
+                    data.lang_code,
+                ),
+                "/",
+            ))
+            .join(&page),
+        )
+        .unwrap();
+        image.read_to_end(&mut buffer).unwrap();
+        writer.start_file(&*page, options).unwrap();
+        writer.write_all(&*buffer).unwrap();
+        buffer.clear();
+        println!("compressed {}", &page);
     }
-    Ok(())
-}
-
-fn strip_characters(original: &str, to_strip: &str) -> String {
-    original
-        .chars()
-        .filter(|&c| !to_strip.contains(c))
-        .collect()
-}
+    writer.finish().unwrap();}
